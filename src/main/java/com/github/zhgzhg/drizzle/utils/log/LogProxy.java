681e8915c219363d09f2214621e31fe3a0a959d3
zhgzhg <perkoz@mail.bg>
2020-06-16 14:17:56 +0300

    Provide warnings channel Improve customizability

--- src/main/java/com/github/zhgzhg/drizzle/utils/log/LogProxy.java
+++ src/main/java/com/github/zhgzhg/drizzle/utils/log/LogProxy.java
@@ -1,43 +1,45 @@
 package com.github.zhgzhg.drizzle.utils.log;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintStream;
 
 public class LogProxy {
+    private static final PrintStream dummyPrintStream = new PrintStream(
+            new OutputStream() { @Override public void write(final int b) throws IOException { }}) { };
+
     public PrintStream stderr() { return System.err; }
 
     public PrintStream stdout() { return System.out; }
 
-    public void cliError(String format, Object... params) {
-        System.err.printf(format, params);
-    }
+    public PrintStream stdwarn() { return System.out; }
 
-    public void cliErrorln() {
-        System.err.println();
-    }
+    public PrintStream stdnull() { return dummyPrintStream; }
 
-    public void cliErrorln(String msg) {
-        System.err.println(msg);
-    }
 
-    public void cliErrorln(Throwable t) {
-        t.printStackTrace(System.err);
-    }
+    public void cliError(String format, Object... params) { stderr().printf(format, params); }
 
-    public void cliInfo(String format, Object... params) {
-        System.out.printf(format, params);
-    }
+    public void cliErrorln() { stderr().println(); }
 
-    public void cliInfoln() {
-        System.out.println();
-    }
+    public void cliErrorln(String msg) { stderr().println(msg); }
 
-    public void cliInfoln(String msg) {
-        System.out.println(msg);
-    }
+    public void cliErrorln(Throwable t) { t.printStackTrace(stderr()); }
 
-    public void cliInfoln(Throwable t) {
-        t.printStackTrace(System.out);
-    }
+    public void cliInfo(String format, Object... params) { stdout().printf(format, params); }
+
+    public void cliInfoln() { stdout().println(); }
+
+    public void cliInfoln(String msg) { stdout().println(msg); }
+
+    public void cliInfoln(Throwable t) { t.printStackTrace(stdout()); }
+
+    public void cliWarn(String format, Object... params) { stdwarn().printf(format, params); }
+
+    public void cliWarnln() { stdwarn().println(); }
+
+    public void cliWarnln(String msg) { stdwarn().println(msg); }
+
+    public void cliWarnln(Throwable t) { t.printStackTrace(stdwarn()); }
 
     public void uiError(String format, Object... params) {
         // to be defined by the UI component if needed
@@ -48,6 +50,10 @@ public class LogProxy {
     }
 
     public void uiInfo(String format, Object... params) {
+        // to be defined by the UI component if needed
+    }
+
+    public void uiWarn(String format, Object... params) {
         // to be defined by the UI component if needed
     }
 }
